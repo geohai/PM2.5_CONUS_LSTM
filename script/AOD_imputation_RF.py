2b9509ed7fe7e0f3e9211784e34f65e96228e733
invictusying <wangzy970301@gmail.com>
2023-11-21 15:44:28 -0700

    Upload RF Imputation Script

--- script/AOD_imputation_RF.py
+++ script/AOD_imputation_RF.py
@@ -34,32 +34,29 @@ def aod_imputation_RF(date,
     # Load the saved stacked data
     unimputed_xr = xarray.open_dataset(f'../data/merged_inputs/{str(date)}_wo_imputation.nc')
 
-    unimputed_xr['ndvi'] = unimputed_xr['ndvi'].interpolate_na(dim='x',
-                                                               method='nearest',
-                                                               fill_value='extrapolate')
+    unimputed_xr['ndvi'] = unimputed_xr['ndvi'].fillna(0)
 
     unimputed_3darray = np.array([
-        unimputed_xr['ndvi'],  # 0
-        unimputed_xr['aod_buffer_047'],  # 1
-        unimputed_xr['aod_buffer_055'],  # 2
-        unimputed_xr['dem'][0],  # 3
+        unimputed_xr['aod_buffer_047'],  # 0
+        unimputed_xr['aod_buffer_055'],  # 1
+        unimputed_xr['day_cos'],  # 2
+        unimputed_xr['day_sin'],  # 3
         unimputed_xr['daymet_dayl'],  # 4
-        unimputed_xr['daymet_prcp'],  # 5
-        unimputed_xr['daymet_srad'],  # 6
-        unimputed_xr['daymet_swe'],  # 7
-        unimputed_xr['gridmet_th'],  # 8
+        unimputed_xr['daymet_lat'],  # 5
+        unimputed_xr['daymet_lon'],  # 6
+        unimputed_xr['daymet_prcp'],  # 7
+        unimputed_xr['daymet_srad'],  # 8
         unimputed_xr['daymet_tmax'],  # 9
         unimputed_xr['daymet_tmin'],  # 10
         unimputed_xr['daymet_vp'],  # 11
-        unimputed_xr['gridmet_vs'],  # 12
-        unimputed_xr['daymet_lat'],  # 13
-        unimputed_xr['daymet_lon'],  # 14
-        unimputed_xr['wildfire_smoke'],  # 15
-        unimputed_xr['year'],  # 16
-        unimputed_xr['day_sin'],  # 17
-        unimputed_xr['day_cos'],  # 18
-        unimputed_xr['month_sin'],  # 19
-        unimputed_xr['month_cos'],  # 20
+        unimputed_xr['dem'][0],  # 12
+        unimputed_xr['gridmet_th'],  # 13
+        unimputed_xr['gridmet_vs'],  # 14
+        unimputed_xr['month_cos'],  # 15
+        unimputed_xr['month_sin'],  # 16
+        unimputed_xr['ndvi'],  # 17
+        unimputed_xr['wildfire_smoke'],  # 18
+        unimputed_xr['year'],  # 19
     ])
 
     aod_3darray = np.array([
@@ -68,22 +65,22 @@ def aod_imputation_RF(date,
     ])
 
     # Get idx of coordinates with buffer
-    model_047_wbuffer_idx = (~np.isnan(unimputed_3darray[1])) & (~np.isnan(unimputed_3darray[2])) \
+    model_047_wbuffer_idx = (~np.isnan(unimputed_3darray[0])) & (~np.isnan(unimputed_3darray[1])) \
                             & (~np.isnan(unimputed_3darray[4])) \
-                            & (~np.isnan(unimputed_3darray[8])) & (np.isnan(aod_3darray[0]))
+                            & (~np.isnan(unimputed_3darray[13])) & (np.isnan(aod_3darray[0]))
 
-    model_055_wbuffer_idx = (~np.isnan(unimputed_3darray[1])) & (~np.isnan(unimputed_3darray[2])) \
+    model_055_wbuffer_idx = (~np.isnan(unimputed_3darray[0])) & (~np.isnan(unimputed_3darray[1])) \
                             & (~np.isnan(unimputed_3darray[4])) \
-                            & (~np.isnan(unimputed_3darray[8])) & (np.isnan(aod_3darray[1]))
+                            & (~np.isnan(unimputed_3darray[13])) & (np.isnan(aod_3darray[1]))
 
     # Get idx of coordinates without buffer
-    model_047_wobuffer_idx = (np.isnan(unimputed_3darray[1])) & (np.isnan(unimputed_3darray[2])) \
+    model_047_wobuffer_idx = (np.isnan(unimputed_3darray[0])) & (np.isnan(unimputed_3darray[1])) \
                              & (~np.isnan(unimputed_3darray[4])) \
-                             & (~np.isnan(unimputed_3darray[8])) & (np.isnan(aod_3darray[0]))
+                             & (~np.isnan(unimputed_3darray[13])) & (np.isnan(aod_3darray[0]))
 
-    model_055_wobuffer_idx = (np.isnan(unimputed_3darray[1])) & (np.isnan(unimputed_3darray[2])) \
+    model_055_wobuffer_idx = (np.isnan(unimputed_3darray[0])) & (np.isnan(unimputed_3darray[1])) \
                              & (~np.isnan(unimputed_3darray[4])) \
-                             & (~np.isnan(unimputed_3darray[8])) & (np.isnan(aod_3darray[1]))
+                             & (~np.isnan(unimputed_3darray[13])) & (np.isnan(aod_3darray[1]))
 
     # Imputation with Buffer
     print('RF models to impute NaNs')
@@ -94,7 +91,7 @@ def aod_imputation_RF(date,
     aod_055_wbuffer_pred = rf_aod_055_wbuffer.predict(unimputed_3darray[:, model_055_wbuffer_idx].T)
 
     # Imputation without Buffer
-    nobuffer_col = np.delete(np.arange(unimputed_3darray.shape[0]), [1, 2])
+    nobuffer_col = np.delete(np.arange(unimputed_3darray.shape[0]), [0, 1])
     # rf_aod_047_wobuffer = joblib.load('../model/saved_model/aod_imputation_model/aod_047_nobuffer.joblib')
     aod_047_wobuffer_pred = rf_aod_047_wobuffer.predict(unimputed_3darray[:, model_047_wobuffer_idx][nobuffer_col, :].T)
 
@@ -113,10 +110,10 @@ def aod_imputation_RF(date,
     unimputed_xr['aod_055'] = (['y', 'x'], aod_3darray[1])
 
     print('Start Exporting')
-    unimputed_xr.to_netcdf(path=f'./outputs/merged_{str(date)}_with_imputation.nc',
+    unimputed_xr.to_netcdf(path=f'../data/merged_inputs/{str(date)}_with_imputation.nc',
                            mode='w',)
     print('Exporting Finished')
-    os.remove(f'./outputs/merged_{str(date)}_wo_imputation.nc')
+    os.remove(f'../data/merged_inputs/{str(date)}_wo_imputation.nc')
     print('Unimputed NetCDF is deleted')
 
 
